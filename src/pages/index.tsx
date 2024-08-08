adcdfd1fdfb5585a4f8bd400d868cb79142d5105
Ilsaf <i.abdulkhakov@innopolis.university>
2024-08-08 20:12:20 +0300

    Add themes and app description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
+import Link from 'next/link';
+import { ModeToggle } from "@/components/ModeToggle";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { ChartNoAxesGantt } from 'lucide-react';
+import { Github } from 'lucide-react';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Flow</h1>
+    <main className="p-4 text-primary">
+        <nav className="pb-2 flex justify-between">
+            <h1 className="flex flex-row gap-x-2 items-center text-xl font-bold"><ChartNoAxesGantt size={24} strokeWidth={2}/> flow</h1>
+            <ModeToggle/>
+        </nav>
+
+        <header className="pt-2">
+            <Card>
+                <CardHeader>
+                    <CardTitle>About app</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p>Flow is an application for organizing notes, tasks, and events.</p>
+                </CardContent>
+                <CardFooter>
+                    <Link href="https://github.com/harutoyume/Flow">
+                        <Button variant="link" className="pl-0 flex gap-x-1"><Github size={20}/>Github</Button>
+                    </Link>
+                </CardFooter>
+            </Card>
+        </header>
     </main>
   );
 }
