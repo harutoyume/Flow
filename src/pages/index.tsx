b29c0fa7ca6e456ed6685f1868d1a9d01ea59626
Ilsaf <i.abdulkhakov@innopolis.university>
2024-08-09 14:05:16 +0300

    Update app description, configure Head properties

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import { ModeToggle } from "@/components/ModeToggle";
 import {
@@ -14,27 +15,32 @@ import { Github } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="p-4 text-primary">
-        <nav className="pb-2 flex justify-between">
-            <h1 className="flex flex-row gap-x-2 items-center text-xl font-bold"><ChartNoAxesGantt size={24} strokeWidth={2}/> flow</h1>
-            <ModeToggle/>
-        </nav>
+      <>
+          <Head>
+              <title>The Flow Organizer</title>
+          </Head>
+          <main className="p-4 text-primary">
+              <nav className="pb-2 flex justify-between">
+                  <h1 className="flex flex-row gap-x-1 items-center text-xl font-bold"><ChartNoAxesGantt size={24} strokeWidth={3}/> flow</h1>
+                  <ModeToggle/>
+              </nav>
 
-        <header className="pt-2">
-            <Card>
-                <CardHeader>
-                    <CardTitle>About app</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p>Flow is an application for organizing notes, tasks, and events.</p>
-                </CardContent>
-                <CardFooter>
-                    <Link href="https://github.com/harutoyume/Flow">
-                        <Button variant="link" className="pl-0 flex gap-x-1"><Github size={20}/>Github</Button>
-                    </Link>
-                </CardFooter>
-            </Card>
-        </header>
-    </main>
+              <header className="pt-2">
+                  <Card>
+                      <CardHeader>
+                          <CardTitle>О приложении</CardTitle>
+                      </CardHeader>
+                      <CardContent>
+                          <p>Flow (Поток) — это приложение для организации заметок, задач и событий.</p>
+                      </CardContent>
+                      <CardFooter>
+                          <Link href="https://github.com/harutoyume/Flow">
+                              <Button variant="link" className="pl-0 flex gap-x-1"><Github size={20}/>Github</Button>
+                          </Link>
+                      </CardFooter>
+                  </Card>
+              </header>
+          </main>
+      </>
   );
 }
