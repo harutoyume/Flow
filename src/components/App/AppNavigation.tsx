a28593f9506ba265c7b2da3a799fe3a2fc20ce32
Ilsaf <i.abdulkhakov@innopolis.university>
2024-08-12 18:16:38 +0300

    Improve menu layout, refactor code

--- src/components/App/AppNavigation.tsx
+++ src/components/App/AppNavigation.tsx
@@ -1,8 +1,16 @@
 import { usePathname } from "next/navigation";
-import React from "react";
-import { ChartNoAxesGantt } from "lucide-react";
-import { ModeToggle } from "@/components/Theme/ModeToggle";
 import Link from "next/link";
+import { ChartNoAxesGantt, Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { ModeToggle } from "@/components/Theme/ModeToggle";
 
 const AppNavigation = () => {
   const pathname = usePathname();
@@ -10,8 +18,8 @@ const AppNavigation = () => {
   return (
     <nav className="z-50 fixed w-full bg-background/80 backdrop-blur-sm">
       <div className="p-3 flex flex-row justify-between items-center">
-        <div className="flex flex-row items-center gap-x-8">
-          <Link href="/" className={`link ${pathname === "/" ? "active" : ""}`}>
+        <div className="flex flex-row items-center gap-x-8 max-sm:hidden">
+          <Link href="/">
             <h1 className="flex flex-row gap-x-1 items-center text-xl font-bold">
               <ChartNoAxesGantt size={24} strokeWidth={3} /> flow{" "}
               <span className="font-light">demo</span>
@@ -38,6 +46,45 @@ const AppNavigation = () => {
             </Link>
           </div>
         </div>
+        <Sheet>
+          <SheetTrigger className="sm:hidden">
+            <Menu />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>
+                <Link href="/">
+                  <h1 className="flex flex-row gap-x-1 items-center text-xl font-bold">
+                    <ChartNoAxesGantt size={24} strokeWidth={3} /> flow{" "}
+                    <span className="font-light">demo</span>
+                  </h1>
+                </Link>
+              </SheetTitle>
+              <SheetDescription>
+                <div className="p-4 flex flex-col gap-y-6 text-left">
+                  <Link
+                    className={`text-sm text-muted-foreground hover:text-primary transition ${pathname === "/tasks" ? "text-primary" : ""}`}
+                    href="/tasks"
+                  >
+                    Задачи
+                  </Link>
+                  <Link
+                    className={`text-sm text-muted-foreground hover:text-primary transition ${pathname === "/events" ? "text-primary" : ""}`}
+                    href="/events"
+                  >
+                    События
+                  </Link>
+                  <Link
+                    className={`text-sm text-muted-foreground hover:text-primary transition ${pathname === "/about" ? "text-primary" : ""}`}
+                    href="/about"
+                  >
+                    О приложении
+                  </Link>
+                </div>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
         <ModeToggle />
       </div>
     </nav>
